172c0d56c67906f8e835fa049e1cca991b4426b8
Andrew Rutherford <2681131+Sidorakh@users.noreply.github.com>
2024-08-18 23:12:36 +1000

    Rebuild

--- vesuvio/param.js
+++
@@ -1,8 +0,0 @@
-var param = new URLSearchParams(window.location.search);
-var param_obj = {};
-for (var pair of param.entries()) {
-    if (document.getElementById(pair[0])) {
-        var elem = document.getElementById(pair[0]);
-        elem.value = pair[1];
-    }
-}
